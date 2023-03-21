11f5d134d185b43d378cf87b45a4c74e188cc167
Ranilson de Andrade Saraiva Júnior <ranilsonjunior2@gmail.com>
2023-03-21 14:19:27 -0300

    banner created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { NavBar } from './components/NavBar';
-// import { Banner } from './components/Banner';
+import { Banner } from './components/Banner';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      {/* <Banner /> */}
+      <Banner />
     </div>
   );
 }
